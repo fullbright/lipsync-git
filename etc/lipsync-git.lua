9e760b592968dcbc132b94dcbd7b0a2987042bc8
full Bright <afanousergio@yahoo.fr>
2012-02-01 00:57:45 +0100

    Adding move, delete, start, and other scripts

---
+++ etc/lipsync-git.lua
@@ -0,0 +1,28 @@
+offlinegit = {
+    delay = 5,
+
+    maxProcesses = 1,
+
+    ---onCreate = "cp -r ^sourcePathname ^targetPathname",
+    onCreate = "/opt/lipsync-git/bin/add-file ^sourcePathname ^source",
+
+    ---onModify = "cp -r ^sourcePathname ^targetPathname",
+    onModify = "/opt/lipsync-git/bin/modify-file ^sourcePathname ^source",
+
+    ---onDelete = "/opt/lipsync-git/bin/del-file ^targetPathname ^source",
+    onDelete = "/opt/lipsync-git/bin/del-file ^sourcePath ^source",
+
+    onMove   = "/opt/lipsync-git/bin/move-file ^o.targetPathname ^d.targetPathname ^source",
+
+    ---onStartup = [[if [ "$(ls -A ^source)" ]; then cp -r ^source* ^target; fi]],
+    onStartup = "/opt/lipsync-git/bin/start-sync ^source",
+}
+
+sync{
+	offlinegit, 
+	source="/home/fullbright/my-lipsync-git/", 
+	host="sergio@home.afanou.com", 
+	targetdir="/home/sergio/mydropbox/",
+	exclude=".git",
+	rsyncOpts="-ltus"
+}
